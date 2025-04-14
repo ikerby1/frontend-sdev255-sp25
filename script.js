f35899d653ef37c876385b5406f752ecadaf3b6b
ikerby1 <ikerby@ivytech.edu>
2025-04-13 23:20:24 -0400

    Updated for Glitch hosting

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //we are going to make an event listener.. it will trigger when the DOM is loaded (aka upon visiting webpage)
 addEventListener("DOMContentLoaded", async function() {
-    const = response = await this.fetch("https://sage-rectangular-regnosaurus.glitch.me:3000/api/songs")
+    const response = await this.fetch("https://sage-rectangular-regnosaurus.glitch.me:3000/api/songs")
     const songs = await response.json()
 
     let html = ""
